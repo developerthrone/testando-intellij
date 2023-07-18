ab0bc0118595f2194576fe8cbad074c7586be287
Jonathan <cavalcantib301@gmail.com>
2023-07-17 22:25:50 -0300

    first commit Intellij.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import model.gato;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        gato gato = new gato();
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello and welcome!" + (a+b));*/
+
+    }
+}
